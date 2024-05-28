a5c2acc34b2fbb97ebb8c9323ee8382da6eba7b8
MeMoChoooo <tokuromanager@gmail.com>
2024-05-29 00:32:52 +0900

    Java Gold勉強進捗 ・第7章　勉強進捗

--- src/main/java/chapter7/GenericsCheckImpl.java
+++ src/main/java/chapter7/GenericsCheckImpl.java
@@ -1,11 +1,33 @@
 package chapter7;
 
+import chapter7.entry.Books;
+import chapter7.entry.Shop;
+import common.ErrorText;
 import common.ResultData;
 
 public class GenericsCheckImpl implements GenericsCheck{
 
     @Override
     public ResultData<Object> execute(testCaseEnum testCase) {
-        return null;
+        ResultData<Object> retData = new ResultData<>();
+        if(!isArgumentError(testCase)){
+            switch (testCase) {
+                case CASE1:
+                    retData.setText(ErrorText.NORMAL_COMPLETE);
+                    retData.setData(new String("テスト"));
+                    break;
+                case CASE2:
+                    retData.setText(ErrorText.NORMAL_COMPLETE);
+                    retData.setData(new Shop<Books>(new Books("技術書")));
+                    break;
+                default:
+                    retData.setText(ErrorText.UNREACHABLE_ERROR);
+                    retData.setData(null);
+                    break;
+            }
+        } else {
+            retData.setText(ErrorText.ARGUMENT_ERROR);
+        }
+        return retData;
     }
 }
